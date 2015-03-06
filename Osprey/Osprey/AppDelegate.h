32e0dd714209db30a3b8349179154f1385ecdd1e
Tyler Fallon <tfallon5@stanford.edu>
2015-03-05 16:41:55 -0800

    Started minimum viable product from proto1

---
+++ Osprey/Osprey/AppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.h
+//  Osprey
+//
+//  Created by Tyler Fallon on 3/5/15.
+//  Copyright (c) 2015 Dixi Chix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+#import "MapViewController.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) MapViewController *mapVC;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Osprey/Osprey/MapViewController.h
@@ -0,0 +1,22 @@
+//
+//  MapViewController.h
+//  Osprey
+//
+//  Created by Tyler Fallon on 3/5/15.
+//  Copyright (c) 2015 Dixi Chix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController {
+    int tapCount;
+    UIImageView *map;
+    UILabel *popExplanation;
+}
+
+- (id)initWithFrame:(CGRect)frame;
+
+@property (strong, nonatomic) NSMutableArray *pindrops;
+
+
+@end
