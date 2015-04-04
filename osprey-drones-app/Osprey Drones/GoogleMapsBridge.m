a626877335d1f1dc8462587fd765710028435d41
rchan2 <rchan2@stanford.edu>
2015-04-03 18:15:23 -0700

    Deleted old osprey drones app

--- osprey-drones-app/Osprey Drones/GoogleMapsBridge.m
+++
@@ -1,9 +0,0 @@
-//
-//  GoogleMapsBridge.m
-//  Osprey Drones
-//
-//  Created by Raymond Chan on 3/12/15.
-//  Copyright (c) 2015 DixiChix. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
