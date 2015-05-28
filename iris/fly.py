a34d0cc94a4e846d9b7e8edb6a6d65420eac977c
sethjhs@stanford.edu <sethjhs@stanford.edu>
2015-05-28 14:30:31 -0700

    again updating to pre crash status, forgot to include fly.py etc

--- iris/fly.py
+++ iris/fly.py
@@ -4,9 +4,74 @@ from pymavlink import mavutil
 
 #python
 import time
-
+import numpy
+import json
+import urllib2
+import urllib
 #raymond
-from get_parse_info import get_location_for_user, get_waypoints_for_user
+# from get_parse_info import get_location_for_user, get_waypoints_for_user
+
+# from look import lookAt
+TAKEOFF_HEIGHT = 15
+
+def get_parse_class_for_username(classname, username):
+    params = urllib.urlencode({
+        "where": json.dumps({
+            "username": username
+        })
+    })
+
+    url = "https://api.parse.com/1/classes/%s?%s" % (classname, params)
+
+    headers = {
+        "X-Parse-Application-Id": "artI8N3mXYFbdiKS1Rl0oMpYgIDQ7YGJJefaevee",
+        "X-Parse-REST-API-Key": "jQncZ8630czaRPuz82zqBtm1PtMqdvJeyY4P4yQJ",
+        "Content-Type": "application/json"
+    }
+
+    request = urllib2.Request(url, None, headers)
+    response = urllib2.urlopen(request)
+
+    json_response = json.loads(response.read())
+    json_results = json_response["results"]
+    return json_results
+
+
+
+def get_waypoints_for_user(username):
+    json_results = get_parse_class_for_username("FlightPath", username)
+
+    # In case there this user somehow hasn't set any waypoints
+    if len(json_results) == 0:
+        return []
+
+    waypoint_strings = json_results[len(json_results) - 1]["waypoints"]
+    result = []
+    for waypoint_string in waypoint_strings:
+        lat_long = tuple([float(coord) for coord in waypoint_string.split(" ")])
+        result.append(lat_long)
+
+    return result
+
+def lookAt(llh,vehicle):
+  msg = vehicle.message_factory.command_long_encode(
+                                                  1, 1,    # target system, target component
+                                                  mavutil.mavlink.MAV_CMD_DO_SET_ROI, #command
+                                                  0, #confirmation
+                                                  0, 0, 0, 0, #params 1-4
+                                                  llh[0],
+                                                  llh[1],
+                                                  llh[2]
+                                                  )
+
+  vehicle.send_mavlink(msg)
+
+def get_user_loc(vehicle):
+	loc = get_waypoints_for_user("ray")[0]
+	locList = list(loc)
+	locList.append(100)
+	lookAt(locList,vehicle)
+	return loc
 
 def change_mode(vehicle, mode):
 	print "Changing to %s mode" % mode
@@ -40,15 +105,15 @@ def takeoff(vehicle):
 	    time.sleep(1)    
 
 def vehicle_at_location(latitude, longitude, vehicle):
-	LOCATION_PRECISION = 9.8081598690443722e-05
+	LOCATION_PRECISION = 9.8081598690443722e-07
 	if numpy.sqrt((latitude-vehicle.location.lat)**2+(longitude-vehicle.location.lon)**2) < LOCATION_PRECISION:
 		return True
 	else:
 		return False
 
-def user_at_location(latitude, longitude, username):
-	LOCATION_PRECISION = 0.00034836762192136367
-	user_location = get_location_for_user(username)
+def user_at_location(latitude, longitude, username,vehicle):
+	LOCATION_PRECISION = 0.00000000348367
+	user_location = get_user_loc(vehicle)
 	# adjust gimbal here
 	if user_location is None:
 		return false
@@ -71,7 +136,8 @@ def main():
 	count = 0
 	while waypoints:
 		count += 1
-		current_waypoint = waypoints.pop(0)
+		current = waypoints.pop(0)
+		current = waypoints.pop(0)
 		latitude = current[0]
 		longitude = current[1]
 		current_location = Location(latitude, longitude, TAKEOFF_HEIGHT, is_relative=True)
@@ -83,7 +149,7 @@ def main():
 			time.sleep(1)
 		print "vehicle at location"
 
-		while not user_at_location(latitude, longitude, "ray"):
+		while not user_at_location(latitude, longitude, "ray",vehicle):
 			time.sleep(1)
 		print "user at location"
 		time.sleep(5)
